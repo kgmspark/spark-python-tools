b84ebbb867499d4dffcdd9fd3af50dddc8f79f03
Katie Greenleaf Martin <136021137+kgmspark@users.noreply.github.com>
2023-07-20 15:22:11 -0400

    Update launchpad bugs create csv.py

--- launchpad bugs create csv.py
+++ launchpad bugs create csv.py
@@ -2,30 +2,34 @@
 """
 Created on Mon Jun 26 14:09:05 2023
 
-@author: direc
+@author: kgmspark
 """
 
 import csv  
 from csv import DictWriter
-from read_csv import read_csv
 
 
 from launchpadlib.launchpad import Launchpad
 
 
+pailsbugs = [
+    1736565,
+    2024610,
+    1667080,
+    2024482,
+    1989368,
+]
 
 field_names = ['number', 'title', 'status', 'target', 'tags']
 
-with open("bugs5.csv", "a", newline='') as f_object:
+with open("bugs.csv", "a", newline='') as f_object:
         dictwriter_object = DictWriter(f_object, fieldnames=field_names)
         dictwriter_object.writeheader()
         f_object.close()
 
 launchpad = Launchpad.login_anonymously('just testing', 'production', version='devel') 
 
-pailsbugs = read_csv('temp2.csv')
-
-for row in pailsbugs:
+for row in pailsbugs :
     
     bug_one = launchpad.bugs[row]
     tasks = bug_one.bug_tasks
@@ -40,7 +44,7 @@ for row in pailsbugs:
 
     dict = {'number': bugid, 'title': title, 'status': status, 'target': milestone, 'tags': tags}
 
-    with open("bugs5.csv", "a", newline='') as f_object:
+    with open("bugs.csv", "a", newline='') as f_object:
         dictwriter_object = DictWriter(f_object, fieldnames=field_names)
         dictwriter_object.writerow(dict)
         f_object.close()
